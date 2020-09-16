9b9cadd3c386b655322a15da62d4a50d4abd0aee
adlerlice <adlerlice@gmail.com>
2020-09-16 08:42:33 +0700

    Исправлены изменения, ничего не работает

--- 11/11/11.cpp
+++ 11/11/11.cpp
@@ -2,13 +2,13 @@
 #include <ctime>
 #include<math.h>
 
-void printInformation(struct Datetime);
-void firstQuestion(struct Datedelta);
+//void printInformation(struct Datetime);
+//void firstQuestion(struct Datedelta);
 void Summa(struct Datetime, struct Datedelta);
 using namespace std;
 
 //хотелось бы создать функцию со структурой, но видимо нет.
-struct Datetime
+struct Datetime //вводим структуру с данными времени
 {
 	int tm_mday;
 	int tm_mon;
@@ -27,22 +27,33 @@ struct Datedelta {
 	int hour;
 };
 
+void Summa(struct Datetime first, struct Datedelta second)
+{
+
+
+	int diffrence;
+
+	diffrence = first.tm_mday + second.day;
+	cout << "Промежуток равен =" << diffrence;
+
+}
+
+
 int main()
 {
 	setlocale(LC_ALL, "Russian");
 	Datetime first;
-	Datedelta second;
+	
 
 	cout << "Введите дату и время" << "\n";
 	cin >> first.tm_mday >> first.tm_mon >> first.tm_year >> first.tm_sec >> first.tm_min >> first.tm_hour;
+	
 
-	printInformation(first);
-	firstQuestion(second);
-	Summa(first, second);
+
 	return 0;
 }
 
-void Datedelta::firstQuestion(struct Datedelta second)
+void Datedelta (struct Datedelta second)
 {
 	second.day = 1;
 	second.mounth = 3;
@@ -53,26 +64,15 @@ void Datedelta::firstQuestion(struct Datedelta second)
 
 }
 
-void Summa(struct Datetime first, struct Datedelta second)
+
+
+/*void printInformation(struct Datetime date)
 {
 
-	Datetime first;
-	Datedelta second;
-	int diffrence;
-
-	diffrence = first.tm_mday + second.day;
-
-
-}
-
-void printInformation(struct Datetime date)
-{
-	Datetime first;
-	Datedelta second;
 	cout << "Your datetime is:" << "\n";
 	/*cout<< date.tm_mday <<"."<< date.tm_mon << "." << date.tm_year << "\n" << date.tm_sec << "." << date.tm_min << ".";
 	cout<< date.tm_hour;*/
-	Summa(first, second);
+	/*Summa(first, second);
 	cout << diffrence;
 	return diffrence;
-}
+}*/
